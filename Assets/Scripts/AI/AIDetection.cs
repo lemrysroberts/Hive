cffc46df999d2b93fc3286af5841cf3c63401025
GKan <kan.gavin@gmail.com>
2013-04-30 21:24:23 +0100

    AI Detection Code
    Script class AIDetection takes Range, FOV and a Target as a parameters,
    if Target is within range and the FOV cone, a raycast then ensures that
    no colliders are in the way.  The usual quick knock-up caveats apply,
    like the range check only considers the middle of the game object, and
    the fact that the hit detection part does nothing.
    
    I've added three debug lines, one is the 'forward' direction, one for
    the Target check and the line then turns blue if a detection has
    occurred.

---
+++ Assets/Scripts/AI/AIDetection.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+public class AIDetection : MonoBehaviour {
+	
+	public GameObject Target;
+	public Material DEBUG_alertMaterial;
+	public float MaxRange = 0; //Range in world units
+	public float FOV = 45;// Field of view in degrees, based on the Gameobjects "left" transform
+	private Material OriginalMaterial;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+		if(Target != null) {
+			float maxRangeSquared = MaxRange * MaxRange;
+			Vector3 targetDirection = Target.transform.position - transform.position;
+			Vector3 myDirection = transform.TransformDirection(Vector3.left);
+			float angleToTarget = Vector3.Dot(Vector3.Normalize(targetDirection), Vector3.Normalize(myDirection));
+			
+			Debug.DrawLine(transform.position, transform.position + Vector3.Normalize(targetDirection) * MaxRange, Color.red);
+			Debug.DrawLine(transform.position, transform.position + myDirection, Color.white);
+			
+			//first part in range, second in viewing cone
+			if(targetDirection.sqrMagnitude < maxRangeSquared && angleToTarget > FOV*Mathf.Deg2Rad){
+				RaycastHit result;
+				if(Physics.Raycast(transform.position, targetDirection, out result, MaxRange) 
+					&& result.collider.gameObject == Target) {
+					//Detected, do stuff or add event or delegate or whatevers
+					Debug.DrawRay(transform.position, targetDirection, Color.blue);
+				}
+				else{
+					//No hit
+				}
+			}
+		}
+	}
+}
