b27fadca0dbd64a5d0fb176ceec89fe4484da024
GKan <kan.gavin@gmail.com>
2013-05-01 20:24:53 +0100

    Added bool to toggle debugging
    I should have done this before, turns off the debugging.  I also
    realised I should at options to change what the "forward" direction is.
    I also fixed a maths fail, I was doubling the FOV by accident.

--- Assets/Scripts/AI/AIDetection.cs
+++ Assets/Scripts/AI/AIDetection.cs
@@ -6,6 +6,7 @@ public class AIDetection : MonoBehaviour {
 	public GameObject Target;
 	public float MaxRange = 0; //Range in world units
 	public float FOV = 45;// Field of view in degrees, based on the Gameobjects "left" transform
+	public bool DrawDebug = false;
 	
 	// Use this for initialization
 	void Start () {
@@ -20,16 +21,20 @@ public class AIDetection : MonoBehaviour {
 			Vector3 myDirection = transform.TransformDirection(Vector3.left);
 			float angleToTarget = Vector3.Dot(Vector3.Normalize(targetDirection), Vector3.Normalize(myDirection));
 			
-			Debug.DrawLine(transform.position, transform.position + Vector3.Normalize(targetDirection) * MaxRange, Color.red);
-			Debug.DrawLine(transform.position, transform.position + myDirection, Color.white);
+			if(DrawDebug){
+				Debug.DrawLine(transform.position, transform.position + Vector3.Normalize(targetDirection) * MaxRange, Color.red);
+				Debug.DrawLine(transform.position, transform.position + myDirection, Color.white);
+			}
 			
 			//first part in range, second in viewing cone
-			if(targetDirection.sqrMagnitude < maxRangeSquared && angleToTarget > FOV*Mathf.Deg2Rad){
+			if(targetDirection.sqrMagnitude < maxRangeSquared && angleToTarget > FOV*Mathf.Deg2Rad/2){
 				RaycastHit result;
 				if(Physics.Raycast(transform.position, targetDirection, out result, MaxRange) 
 					&& result.collider.gameObject == Target) {
 					//Detected, do stuff or add event or delegate or whatevers
-					Debug.DrawRay(transform.position, targetDirection, Color.blue);
+					if(DrawDebug){
+						Debug.DrawRay(transform.position, targetDirection, Color.blue);
+					}
 				}
 				else{
 					//No hit
